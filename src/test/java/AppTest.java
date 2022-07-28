071e19dcec78fef776f4d78197f479b96c6d3d87
EunJung803 <aimee3322@gmail.com>
2022-07-28 21:55:23 +0900

    jsonStr Test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,4 +1,5 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,10 @@ public class AppTest {
     @Test
     void 실험_ObjectMapper() throws JsonProcessingException {
         ArticleDto articleDto = new ArticleDto(1, "제목", "내용");
+
+//        ObjectMapper om = new ObjectMapper();
+//        String jsonStr = om.writeValueAsString(articleDto);
+//        System.out.println(jsonStr);
 
         String jsonStr = Ut.json.toJsonStr(articleDto, "");
         assertThat(jsonStr).isNotBlank();
