1d45c9e8cfb61fa228edda86e849013a62bf2ccd
EunJung803 <aimee3322@gmail.com>
2022-07-31 23:22:30 +0900

    AppTest 주석 추가

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -18,6 +18,7 @@ public class AppTest {
     }
 
     @Test
+        // ArticleDto => JS객체(단순)
     void ObjectMapper__articleDtoToJsonStr() {
         ArticleDto articleDto = new ArticleDto(1, "제목", "내용");
 
@@ -29,6 +30,7 @@ public class AppTest {
     }
 
     @Test
+        // List<ArticleDto> => JS배열
     void ObjectMapper__articleDtoListToJsonStr() {
         List<ArticleDto> articleDtos = new ArrayList<>();
         articleDtos.add(new ArticleDto(1, "제목1", "내용1"));
@@ -41,6 +43,7 @@ public class AppTest {
     }
 
     @Test
+        // Map<String, ArticleDto> => JS객체(복잡)
     void ObjectMapper__articleDtoMapToJsonStr() {
         Map<String, ArticleDto> articleDtoMap = new HashMap<>();
         articleDtoMap.put("가장오래된", new ArticleDto(1, "제목1", "내용1"));
@@ -52,6 +55,7 @@ public class AppTest {
     }
 
     @Test
+        // JS객체(단순) => ArticleDto
     void ObjectMapper__jsonStrToObj() {
         ArticleDto articleDtoOrigin = new ArticleDto(1, "제목", "내용");
         String jsonStr = Ut.json.toStr(articleDtoOrigin, "");
@@ -62,6 +66,7 @@ public class AppTest {
     }
 
     @Test
+        // JS배열 => List<ArticleDto>
     void ObjectMapper__jsonStrToArticleDtoList() {
         List<ArticleDto> articleDtos = new ArrayList<>();
         articleDtos.add(new ArticleDto(1, "제목1", "내용1"));
